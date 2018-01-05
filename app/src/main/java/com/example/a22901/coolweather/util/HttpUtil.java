39f96239fd5b90b16a2a8c1d3c6adb68187fe526
NoNameisOK <2290151272@qq.com>
2018-01-05 16:19:08 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/example/a22901/coolweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.example.a22901.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by 22901 on 2018/1/5.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
